4f075b89051cbe551268eb77098149d04eda210e
alissonif <josealissonif@gmail.com>
2024-02-13 17:04:50 -0300

    favicon.png 30.15

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,6 @@ export function App() {
       <div>
         <h1 className="text-3xl font-bold underline">Hello world!</h1>
       </div>
-      <div>
-        <h2>teste</h2>
-      </div>
     </>
   );
 }
