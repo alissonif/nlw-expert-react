3e15891aa397dd9fd141b01c4a173b755e8d8b20
alissonif <josealissonif@gmail.com>
2024-02-13 14:03:20 -0300

    first commint

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+export function App() {
+  return (
+    <>
+      <div>
+        <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      </div>
+      <div>
+        <h2>teste</h2>
+      </div>
+    </>
+  );
+}
