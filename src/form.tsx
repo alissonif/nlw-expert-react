064dd3df4a045dc4de981a262fe8ccdb642a5d90
alissonif <josealissonif@gmail.com>
2024-02-13 21:25:05 -0300

    feat: Implementing the screen main of Notes | Finish lesson 1

---
+++ src/form.tsx
@@ -0,0 +1,11 @@
+export function Form() {
+  return (
+    <form className="w-full">
+      <input
+        type="text"
+        placeholder="Busque as suas notas..."
+        className="w-full bg-transparent text-3xl font-semibold tracking-tight outline-none placeholder:text-slate-500"
+      />
+    </form>
+  );
+}
