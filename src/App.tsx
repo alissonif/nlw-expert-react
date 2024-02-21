cc6e75eef6aa156c04eb8a701569738cb61b4c04
alissonif <josealissonif@gmail.com>
2024-02-21 00:28:02 -0300

    feat: Implementing API Google SpeechRecognition in Browser | finish completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,50 @@
-import { formatDistanceToNow } from "date-fns";
-import { ptBR } from "date-fns/locale";
 import logo from "./assets/logo-nlw-expert.svg";
 import { NewNoteCard } from "./components/new-note-card";
 import { NoteCard } from "./components/note-card";
-const note = {
-  date: new Date(),
-  content: "Hello world",
+import { ChangeEvent, useState } from "react";
+type Note = {
+  id: string;
+  date: Date;
+  content: string;
 };
-{
-  formatDistanceToNow(note.date, {
-    locale: ptBR,
-    addSuffix: true,
-  });
-}
 export function App() {
+  const [search, setSearch] = useState("");
+  const [notes, setNotes] = useState<Note[]>(() => {
+    const storagedNotes = localStorage.getItem("notes");
+    if (storagedNotes) {
+      return JSON.parse(storagedNotes);
+    }
+    return [];
+  });
+  function onNoteCreated(content: string) {
+    const newNote = {
+      id: Math.random(),
+      // id: crypto.randomUUID(),
+      date: new Date(),
+      content: content,
+    };
+    const notesArray = [newNote, ...notes];
+    setNotes(notesArray);
+    localStorage.setItem("notes", JSON.stringify(notesArray));
+  }
+  function onNoteDeleted(id: string) {
+    const notesArray = notes.filter((note) => note.id !== id);
+    setNotes(notesArray);
+    localStorage.setItem("notes", JSON.stringify(notesArray));
+  }
+  function handleSearch(event: ChangeEvent<HTMLInputElement>) {
+    const query = event.target.value;
+    setSearch(query);
+  }
+  // console.log(search);
+  const filteredNotes =
+    search !== ""
+      ? notes.filter((note) =>
+          note.content.toLocaleLowerCase().includes(search.toLocaleLowerCase()),
+        )
+      : notes;
   return (
-    <div className="mx-auto max-w-6xl my-12 space-y-6">
+    <div className="mx-auto max-w-6xl my-12 space-y-6 px-5">
       <img src={logo} alt="NLW Expert" />
 
       <form className="w-full">
@@ -23,15 +52,17 @@ export function App() {
           type="text"
           placeholder="Busque as suas notas..."
           className="w-full bg-transparent text-3xl font-semibold tracking-tight outline-none placeholder:text-slate-500"
+          onChange={handleSearch}
         />
       </form>
 
       <div className="h-px bg-slate-700" />
 
-      <div className="grid grid-cols-3 auto-rows-[250px] gap-6">
-        <NewNoteCard note={note} />
-        <NoteCard note={note} />
-        <NoteCard note={note} />
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 auto-rows-[250px] gap-6">
+        <NewNoteCard onNoteCreated={onNoteCreated} />
+        {filteredNotes.map((note) => (
+          <NoteCard key={note.id} note={note} onNoteDeleted={onNoteDeleted} />
+        ))}
       </div>
     </div>
   );
--- src/components/note-card.tsx
+++ src/components/note-card.tsx
@@ -5,16 +5,21 @@ import { X } from "lucide-react";
 
 type NoteCardProps = {
   note: {
+    id: string;
     date: Date;
     content: string;
   };
+  onNoteDeleted: (id: string) => void;
 };
-export function NoteCard({ note, ...rest }: NoteCardProps) {
+export function NoteCard({ note, onNoteDeleted, ...rest }: NoteCardProps) {
   return (
     <Dialog.Root>
       <Dialog.Trigger className="rounded-md text-left bg-slate-800 flex flex-col p-5 gap-3 overflow-hidden relative hover:ring-2 hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-400 outline-none">
         <span className="text-sm font-medium text-slate-300">
-          {note.date.toString()}
+          {formatDistanceToNow(note.date, {
+            locale: ptBR,
+            addSuffix: true,
+          })}
         </span>
         <p className="text-sm leading-6 text-slate-400 ">{note.content}</p>
         <div className="absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0 pointer-events-none" />
@@ -35,6 +40,7 @@ export function NoteCard({ note, ...rest }: NoteCardProps) {
             <p className="text-sm leading-6 text-slate-400 "> {note.content}</p>
           </div>
           <button
+            onClick={() => onNoteDeleted(note.id)}
             type="button"
             className="bg-slate-800 w-full py-4 text-center text-sm text-slate-300 outline-none font-medium group"
           >
